8251ed3bd985fa267454ae35fa153bb332f9ceee
wangmiaozero <1061214467@qq.cpm>
2020-08-17 00:11:54 +0800

    初探

---
+++ src/pages/test-page/test-page.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		test-page
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
